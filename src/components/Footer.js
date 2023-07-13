1e50a8bc83ee0716264ece55b6c8ab76ef516762
Kimi Nel <kimi.monique.nel@gmail.com>
2023-07-13 14:51:23 +0200

    style: footer completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <footer className="fixed bottom-0 z-10 bg-fill w-full h-24 md:h-16 text-xl shadow-footer">
       <div className="grid grid-cols-2 md:grid-cols-3 py-4 w-full">
-        <div className="flex gap-x-8 md:col-span-1 pl-8">
+        <div className="flex gap-x-8 md:col-span-1 pl-8 items-center">
           <Link
             to={"https://github.com/kiminel"}
             className="hover:-translate-y-1 transition duration-200"
@@ -33,18 +33,18 @@ const Footer = () => {
           </Link>
         </div>
 
-        <div className="col-span-1 md:col-span-1 flex justify-end pr-8 md:justify-center">
+        <div className="col-span-1 md:col-span-1 flex justify-end pr-4 md:pr-0 md:justify-center text-center self-center">
           <Link
             to={"/about"}
-            className="transition duration-200 bg-dark text-fill rounded-lg mr-4 px-3 py-1 border-[2px] hover:bg-fill hover:text-dark font-semibold"
+            className="transition duration-200 bg-dark text-fill rounded-lg mr-4 px-2 py-1 text-base md:px-3 md:py-1 md:text-xl border-[2px] hover:bg-fill hover:text-dark font-semibold"
             target="_blank"
           >
             About
           </Link>
         </div>
 
-        <div className="text-lg col-span-3 md:col-span-1 flex justify-start pl-8 pt-2 md:pt-0 md:justify-end md:pr-8">
-          {new Date().getFullYear()} &copy; All Rights Reserve
+        <div className="text-xs col-span-3 md:col-span-1 flex self-center justify-start pl-8 pt-2 md:pt-0 md:justify-end md:pr-8">
+          {new Date().getFullYear()} &copy; All Rights Reserved
         </div>
       </div>
     </footer>
