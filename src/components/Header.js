eadc5ecf515e638c3a812de224aca2405711b36d
Kimi Nel <kimi.monique.nel@gmail.com>
2023-07-13 19:42:19 +0200

    style: responsive update

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import NavLinks from "./NavLinks";
 import { Link } from "react-router-dom";
 import { graphcms, QUERY_SLUG_CATEGORIES } from "../GraphQL/Queries";
-import Search from "./Search";
 import HamburgerMenu from "./icons/HamburgerMenu";
 import SideBar from "./SideBar";
 
@@ -18,8 +17,8 @@ const Header = () => {
   }, []);
 
   return (
-    <header className="fixed top-0 z-50 bg-fill w-full h-24 shadow-lg grid grid-cols-2">
-      <Link className="col-span-1 flex" to="/">
+    <header className="fixed top-0 z-50 bg-fill w-full h-24 shadow-lg grid grid-cols-3 md:grid-cols-4">
+      <Link className="col-span-2 flex" to="/">
         <img
           className="w-auto h-24 ml-2"
           src={"/images/SA-flag.png"}
@@ -27,16 +26,16 @@ const Header = () => {
         />
         <div className="sm:text-lg md:text-xl xlg:text-3xl self-center pl-2">
           <h2 className="font-bold">South African Food Blog</h2>
-          <span className="font-style: italic">Eats Lekker! </span>
+          <span className="font-style:italic">Eats Lekker! </span>
         </div>
       </Link>
 
-      <div className="col-span-1 hidden md:flex justify-evenly self-center text-md lg:text-xl">
+      <div className="col-span-1 md:col-span-2 hidden md:flex text-center justify-center items-center text-md lg:text-xl">
         <NavLinks categories={categories} />
       </div>
-      <div className=" flex items-center">
+      <div className="flex items-center">
         <button
-          className="absolute right-12 items-center justify-center mt-2 flex md:hidden"
+          className="absolute right-12 mt-2 flex md:hidden"
           onClick={() => setIsOpen(!isOpen)}
         >
           <HamburgerMenu className={"h-10 w-10"} />
